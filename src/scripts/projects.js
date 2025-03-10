12dc951df1b73cf8b971bf85ff0f95c1ddc8f8c8
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2025-03-10 17:49:42 -0500

    Agregar "skills" and "projects" objects

---
+++ src/scripts/projects.js
@@ -0,0 +1,42 @@
+const projects = [
+  {
+    title: "Plantory",
+    description:
+      "A simple app to manage plant nursery inventory, track stock, and organize plants",
+    projectImgSrc: "./src/images/plantory-screen.jpg",
+    altImgSrc: "plantory app screenshot",
+    linkSrc: "https://inventory-app-jg.up.railway.app/",
+    githubSrc: "https://github.com/jacob-guerrero/odin-inventory-app",
+    altExternalLinks: "External link",
+  },
+  {
+    title: "Shopping Cart",
+    description:
+      "A React-based shopping cart application that allows users to browse products, add items to their cart, and manage quantities.",
+    projectImgSrc: "./src/images/shopping-screen.jpg",
+    altImgSrc: "shopping cart app screenshot",
+    linkSrc: "https://shopping-cart-jg.netlify.app/",
+    githubSrc: "https://github.com/jacob-guerrero/odin-shopping-cart",
+    altExternalLinks: "External link",
+  },
+  {
+    title: "The Purrfect Memory",
+    description:
+      "The Purrfect Memory is a React-based memory game where players click unique cat images without repeating selections.",
+    projectImgSrc: "./src/images/memory-screen.jpg",
+    altImgSrc: "Memory card app screenshot",
+    linkSrc: "https://memory-card-jg.netlify.app/",
+    githubSrc: "https://github.com/jacob-guerrero/odin-memorycard",
+    altExternalLinks: "External link",
+  },
+  {
+    title: "Weather App",
+    description:
+      "A simple and interactive weather forecast application that fetches real-time weather data using the Weather API.",
+    projectImgSrc: "./src/images/weather-screen.jpg",
+    altImgSrc: "Weather app screenshot",
+    linkSrc: "https://jacob-guerrero.github.io/odin-weather-app/",
+    githubSrc: "https://github.com/jacob-guerrero/odin-weather-app",
+    altExternalLinks: "External link",
+  },
+];
---
+++ src/scripts/skills.js
@@ -0,0 +1,80 @@
+const skills = [
+  {
+    title: "HTML5",
+    src: "./src/icons/html.png",
+    alt: "html icon",
+  },
+  {
+    title: "CSS3",
+    src: "./src/icons/css.png",
+    alt: "css icon",
+  },
+  {
+    title: "JavaScript",
+    src: "./src/icons/js.png",
+    alt: "JavaScript icon",
+  },
+  {
+    title: "React",
+    src: "./src/icons/react.svg",
+    alt: "react icon",
+  },
+  {
+    title: "NodeJS",
+    src: "./src/icons/nodejs.svg",
+    alt: "nodejs icon",
+  },
+  {
+    title: "ExpressJS",
+    src: "./src/icons/express.png",
+    alt: "express icon",
+  },
+  {
+    title: "Python",
+    src: "./src/icons/python.png",
+    alt: "python icon",
+  },
+  {
+    title: "PostgreSQL",
+    src: "./src/icons/postgresql.png",
+    alt: "postgresql icon",
+  },
+];
+
+const additionalSkills = [
+  {
+    title: "Bootstrap",
+    src: "./src/icons/bootstrap.svg",
+    alt: "bootstrap icon",
+  },
+  {
+    title: "Tailwind",
+    src: "./src/icons/tailwind.svg",
+    alt: "tailwind icon",
+  },
+  {
+    title: "Material UI",
+    src: "./src/icons/materialui.svg",
+    alt: "materialui icon",
+  },
+  {
+    title: "Figma",
+    src: "./src/icons/figma.svg",
+    alt: "figma icon",
+  },
+  {
+    title: "Git",
+    src: "./src/icons/git.svg",
+    alt: "git icon",
+  },
+  {
+    title: "Dbeaver",
+    src: "./src/icons/dbeaver.png",
+    alt: "dbeaver icon",
+  },
+  {
+    title: "Django",
+    src: "./src/icons/django.svg",
+    alt: "django icon",
+  },
+];
