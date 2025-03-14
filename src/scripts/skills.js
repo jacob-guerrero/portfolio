071b8ff2fd14aa0e21cb162c3c7835b72faae995
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2025-03-14 00:41:09 -0500

    Agregar skills usando JS

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -28,6 +28,7 @@ const skills = [
     title: "ExpressJS",
     src: "./src/icons/express.png",
     alt: "express icon",
+    imgClass: "w-15 h-10 bg-white px-2 my-2.5",
   },
   {
     title: "Python",
@@ -76,5 +77,35 @@ const additionalSkills = [
     title: "Django",
     src: "./src/icons/django.svg",
     alt: "django icon",
+    imgClass: "w-25 h-15",
   },
 ];
+
+function createSkills(arr, parent) {
+  arr.forEach((skill) => {
+    const liContainer = document.createElement("li");
+    const imgSkill = document.createElement("img");
+    const pSkill = document.createElement("p");
+
+    /* li container */
+    liContainer.className =
+      "w-full bg-deep-blue rounded-lg flex flex-col gap-3 justify-center items-center px-4 py-6";
+
+    /* img skill */
+    imgSkill.src = `${skill.src}`;
+    imgSkill.alt = `${skill.alt}`;
+    imgSkill.className = !skill.imgClass ? "w-15 h-15" : `${skill.imgClass}`;
+
+    /* div text content */
+    pSkill.textContent = `${skill.title}`;
+    pSkill.className = "text-xl";
+
+    /* Append elements */
+    liContainer.append(imgSkill, pSkill);
+
+    parent.appendChild(liContainer);
+  });
+}
+
+const skillsContainer = document.getElementById("skills_container");
+createSkills(skills, skillsContainer);
